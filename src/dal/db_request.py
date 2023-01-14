e4381fd738ac9e65fd62972a54686088a9e5b71c
Tommy Yellin <tommyellin365@gmail.com>
2023-01-14 22:04:10 +0200

    boto3 imp

--- src/dal/db_request.py
+++ src/dal/db_request.py
@@ -1,9 +1,18 @@
+import boto3
+
+
 class DBRequest:
     def __init__(self, ac: str, sc: str):
         self.__access_key = ac
         self.__secret_key = sc
 
     def create_table(self, primary_key_name: str, primary_key_type: str, table_name: str):
+        client = boto3.client(
+            'dynamodb',
+            region_name="eu-west-1",
+            aws_access_key_id=self.__access_key,
+            aws_secret_access_key=self.__secret_key
+        )
         params = {
             'TableName': table_name,
             'KeySchema': [
@@ -11,13 +20,14 @@ class DBRequest:
             ],
             'AttributeDefinitions': [
                 {'AttributeName': primary_key_name, 'AttributeType': primary_key_type}
-            ]
+            ],
+            'BillingMode': 'PAY_PER_REQUEST'
         }
-        print(params)
-        # TODO: call boto3 create table
+        response = client.create_table(**params)
+        print(response)
 
 
 if __name__ == "__main__":
-    pass
-    # db_request = DBRequest('sexrdctfn', 'drcftbhunj')
-    # db_request.create_table('dog_id', 'N', 'Doggies')
+
+    db_request = DBRequest('accessKey', 'secretKey')
+    db_request.create_table('dog_id', 'N', 'Doggies')
